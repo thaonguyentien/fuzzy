743994af2b9a3baaef70d1305f852e897472ab80
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-04-30 19:16:52 +0700

    multi inter

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -4,64 +4,72 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
-    public class CarUserControl : MonoBehaviour
-    {
-        private CarController m_Car; // the car controller we want to use
+	[RequireComponent(typeof (CarController))]
+	public class CarUserControl : MonoBehaviour
+	{
+		private CarController m_Car; // the car controller we want to use
 		public GameObject car;
-		public GameObject inters;
+		public GameObject inter;
 		float dist_pre = 999999f;
-		bool isTurn=false;
-		bool turnDone=false;
-        private void Awake()
-        {
-            // get the car controller
-            m_Car = GetComponent<CarController>();
-			inters[0] = GameObject.Find("Inter1");
-			inters[1] = GameObject.Find("Inter2");
+		bool[] isTurns=new bool[20];
+		bool[] turnDones=new bool[20];
+		public GameObject[] inters= new GameObject[20];
+		private Vector3[] interPositions = new Vector3[20]; 
+		int index=0;
+		private void Awake()
+		{
+			// get the car controller
+			m_Car = GetComponent<CarController>();
+			inters= GameObject.FindGameObjectsWithTag("inter");
+			//			inters.Add (inter);
+			//			inters[1] = GameObject.Find("Inter2");
 			car = GameObject.Find("Car");
-        }
+			interPositions[0] = inters[0].transform.position;
+			interPositions[1] = inters[1].transform.position;
+		}
 
 
-        private void FixedUpdate()
-        {
-			
+		private void FixedUpdate()
+		{
+
 			float h = 0;// am re phai, duong re trai
 			float v = 1;
-//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
-//			float v = CrossPlatformInputManager.GetAxis("Vertical");
+			//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			//			float v = CrossPlatformInputManager.GetAxis("Vertical");
 			Vector3 carPosition = car.transform.position;
-			Vector3 interPosition = inters.transform.position;
-			float dist = Vector3.Distance(carPosition, interPosition);
 
-//			print("Distance to other: " + dist);
+
+			float dist = Vector3.Distance(carPosition, interPositions[index]);
+
+			//			print("Distance to other: " + dist);
 			print (m_Car.CurrentSpeed +" h: " + h + " v: "+ v +"distance: "+ dist + " m_Car.transform.eulerAngles: "+ m_Car.transform.eulerAngles.y);
-//			print (car.transform.position.x);
-			if (dist<=23f && (turnDone == false)) {
+			//			print (car.transform.position.x);
+			if (dist<=23f && (turnDones[index] == false)) {
 				v = 0;
 				h = 0.4f;
-				isTurn = true;
+				isTurns[index] = true;
 
 			} 
-			if (dist>(dist_pre) && dist>20f && isTurn) {
+			if (dist>(dist_pre) && dist>20f && isTurns[index]) {
 				m_Car.transform.eulerAngles=  (new Vector3 (0, 90, 0));
 				v = 1;
 				h = 0;
-				turnDone = true;
-//				isTurn = false;
+				turnDones[index] = true;
+				index++;
+				//				isTurn = false;
 			}
 
 			dist_pre = dist;
-            // pass the input to the car!
-           
+			// pass the input to the car!
+
 			print (h);
-//			print (v);
-#if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
-        }
-    }
+			//			print (v);
+			#if !MOBILE_INPUT
+			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+			m_Car.Move(h, v, v, handbrake);
+			#else
+			m_Car.Move(h, v, v, 0f);
+			#endif
+		}
+	}
 }
