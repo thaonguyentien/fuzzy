5ce08a7ce8e42167f74f05fccac236191e86977f
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-05-01 17:37:21 +0700

    them road 5

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -9,7 +9,7 @@ namespace UnityStandardAssets.Vehicles.Car
 	{
 		private CarController m_Car; // the car controller we want to use
 		public GameObject car;
-//		public GameObject inter;
+		//		public GameObject inter;
 		float dist_pre = 999999f;
 		bool[] isTurns=new bool[20];
 		bool[] turnDones=new bool[20];
@@ -28,7 +28,7 @@ namespace UnityStandardAssets.Vehicles.Car
 			car = GameObject.Find("Car");
 			for (int i=0;i<inters.Length;++i) {
 				interPositions[i] = inters[i].transform.position;
-//				print(interPositions[i]);
+				//				print(interPositions[i]);
 			}
 
 
@@ -46,30 +46,104 @@ namespace UnityStandardAssets.Vehicles.Car
 
 
 			float dist = Vector3.Distance(carPosition, interPositions[index]);
-//			print (index+" : " +interPositions[index]);
-			//			print("Distance to other: " + dist);
-//			print (m_Car.CurrentSpeed +" h: " + h + " v: "+ v +"distance: "+ dist + " m_Car.transform.eulerAngles: "+ m_Car.transform.eulerAngles.y);
-			//			print (car.transform.position.x);
-			if (dist<=23f && (turnDones[index] == false)) {
-				v = 0;
-				h = 0.4f;
-				isTurns[index] = true;
+			print (index+" : " +interPositions[index]);
 
-			} 
-			if (dist>(dist_pre) && dist>20f && isTurns[index]) {
-				m_Car.transform.eulerAngles=  (new Vector3 (0, 90, 0));
-				v = 1;
-				h = 0;
-				print ("turn: " + index);
-				turnDones[index] = true;
-				index--;
-				//				isTurn = false;
+			//			print("Distance to other: " + dist);
+			//			print (" h: " + h + " v: "+ v +"distance: "+ dist + " pre_dist:" + dist_pre + " isTurn " + isTurns[index] );
+			//			print (car.transform.position.x);
+			//			print("dist: " +dist + "turnDones: "+  turnDones[index]); 
+
+			if (interPositions [index].x < interPositions [index - 1].x) {//turn right x
+				if (dist<=23f && (turnDones[index] == false)) {
+					v = 0;
+					h = 0.36f;
+					isTurns[index] = true;
+					print ("dang re");
+				} 
+				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+					m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
+					v = 1;
+					h = 0;
+					print ("turn right x: " + index);
+					turnDones [index] = true;
+					index--;
+					if (index < 0) {
+						Time.timeScale = 0;
+					}
+					dist_pre = 999999f;
+					print ("turnDone");
+					//				isTurn = false;
+				}
+			} else if (interPositions [index].x > interPositions [index - 1].x){// turn left x
+				if (dist<=23f && (turnDones[index] == false)) {
+					v = 0;
+					h = -0.36f;
+					isTurns[index] = true;
+					print ("dang re");
+				} 
+				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+					m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
+					v = 1;
+					h = 0;
+					print ("turn left x: " + index);
+					turnDones [index] = true;
+					index--;
+					if (index < 0) {
+						Time.timeScale = 0;
+					}
+					dist_pre = 999999f;
+					print ("turnDone");
+					//				isTurn = false;
+				}
+			}else if (interPositions [index].z < interPositions [index - 1].z){// turn left z
+				if (dist<=23f && (turnDones[index] == false)) {
+					v = 0;
+					h = -0.36f;
+					isTurns[index] = true;
+					print ("dang re");
+				} 
+				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+					m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
+					v = 1;
+					h = 0;
+					print ("turn left z: " + index);
+					turnDones [index] = true;
+					index--;
+					if (index < 0) {
+						Time.timeScale = 0;
+					}
+					dist_pre = 999999f;
+					print ("turnDone");
+					//				isTurn = false;
+				}
+			}else if (interPositions [index].z > interPositions [index - 1].z){// turn right z
+				if (dist<=23f && (turnDones[index] == false)) {
+					v = 0;
+					h = 0.36f;
+					isTurns[index] = true;
+					print ("dang re");
+				} 
+				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+					m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
+					v = 1;
+					h = 0;
+					print ("turn right z: " + index);
+					turnDones [index] = true;
+					index--;
+					if (index < 0) {
+						Time.timeScale = 0;
+					}
+					dist_pre = 999999f;
+					print ("turnDone");
+					//				isTurn = false;
+				}
 			}
+
 
 			dist_pre = dist;
 			// pass the input to the car!
 
-			print (h);
+			//			print (h);
 			//			print (v);
 			#if !MOBILE_INPUT
 			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
