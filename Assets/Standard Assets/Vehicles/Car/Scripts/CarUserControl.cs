fdc3dfec4f3689e23d4b3a2ff39f2ddaff15e2c3
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-05-07 00:40:34 +0700

    re thang

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -10,6 +10,10 @@ namespace UnityStandardAssets.Vehicles.Car
 	[RequireComponent(typeof (CarController))]
 	public class CarUserControl : MonoBehaviour
 	{
+		float h = 0;// am re phai, duong re trai
+		float v = 1;
+
+		bool huong_z=true;
 		int len=-1;
 		public Dropdown m_Dropdown;
 		bool flag_inter=false;
@@ -32,7 +36,7 @@ namespace UnityStandardAssets.Vehicles.Car
 		int index=0;
 		Vector3 carPosition;
 		int[] checkpoints = new int[20];
-
+		float dist;
 		private void Awake()
 		{
 			ShowOverView ();
@@ -53,173 +57,168 @@ namespace UnityStandardAssets.Vehicles.Car
 			if (m_Dropdown.value != 0) {
 				Destroy (m_Dropdown);
 				StartCoroutine(GetMap(0,m_Dropdown.value));
-				print ("target:" + m_Dropdown.value);
-
-
 
 				if(len!=-1)	{
-					print (checkpoints.Length);
-						if (flag_inter == false) {
-							//					
-							//					inters = GameObject.FindGameObjectsWithTag ("inter");
-							//					index = inters.Length - 1;
-							//					flag_inter = true;
-							//					for (int i=0;i<inters.Length;++i) {
-							//						 = inters[i].transform.position;
-							//						print(inters[i].transform.position);
-							//					}
-						for(int i=0;i<len;i++){
-							print(checkpoints[i]);
-							String name = "Inter" + checkpoints[i];
-							GameObject point = GameObject.Find (name);
-							print ("point "+point.transform.position);
-							interPositions[i]=point.transform.position;
-							print (point.transform.position);
-							point.gameObject.tag = "inter";
-								
-							}
-						}
-						if (flag_chuong_ngai_vat == true) {
+	//					print ("checkpoints length: " + checkpoints.Length);
+					if (flag_inter == false) {
+						//					
+						//					inters = GameObject.FindGameObjectsWithTag ("inter");
+						//					index = inters.Length - 1;
+						//					flag_inter = true;
+						//					for (int i=0;i<inters.Length;++i) {
+						//						 = inters[i].transform.position;
+						//						print(inters[i].transform.position);
+						//					}
+					for(int i=0;i<len;i++){
+	//						print(checkpoints[i]);
+						String name = "Inter" + checkpoints[i];
+						GameObject point = GameObject.Find (name);
+						print ("point "+point.transform.position);
+						interPositions[i]=point.transform.position;
+	//						print (point.transform.position);
+						point.gameObject.tag = "inter";
 							
-							timeLeft -= Time.deltaTime;
-							timer.GetComponent<Text> ().text = " " + timeLeft.ToString ();
-						} else {
-							timer.GetComponent<Text> ().text = " ";
 						}
-						ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-						if (timeLeft < 0) {
-							GameObject cnt = GameObject.Find ("chuong_ngai_vat(Clone)");
-							print ("destroy");
-							Destroy (cnt);
-							timeLeft = 10.0f;
-							flag_chuong_ngai_vat = false;
+					}
+					if (flag_chuong_ngai_vat == true) {
+						
+						timeLeft -= Time.deltaTime;
+						timer.GetComponent<Text> ().text = " " + timeLeft.ToString ();
+					} else {
+						timer.GetComponent<Text> ().text = " ";
+					}
+					ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+					if (timeLeft < 0) {
+						GameObject cnt = GameObject.Find ("chuong_ngai_vat(Clone)");
+						print ("destroy");
+						Destroy (cnt);
+						timeLeft = 10.0f;
+						flag_chuong_ngai_vat = false;
+					}
+					if (Physics.Raycast (ray, out hit)) {
+
+						if (Input.GetKey (KeyCode.Mouse0) && flag_chuong_ngai_vat == false) {
+							GameObject obj = Instantiate (chuong_ngai_vat, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+							flag_chuong_ngai_vat = true;
+
 						}
-						if (Physics.Raycast (ray, out hit)) {
 
-							if (Input.GetKey (KeyCode.Mouse0) && flag_chuong_ngai_vat == false) {
-								GameObject obj = Instantiate (chuong_ngai_vat, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
-								flag_chuong_ngai_vat = true;
-
-							}
-
-						}
-
-						float h = 0;// am re phai, duong re trai
-						float v = 1;
-						//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
-						//			float v = CrossPlatformInputManager.GetAxis("Vertical");
-						carPosition = car.transform.position;
+					}
 
 
-						float dist = Vector3.Distance (carPosition, interPositions [index]);
-						print (index + " : " + interPositions [index]);
+					//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+					//			float v = CrossPlatformInputManager.GetAxis("Vertical");
+					carPosition = car.transform.position;
 
-						//			print("Distance to other: " + dist);
-						//			print (" h: " + h + " v: "+ v +"distance: "+ dist + " pre_dist:" + dist_pre + " isTurn " + isTurns[index] );
-						//			print (car.transform.position.x);
-						//			print("dist: " +dist + "turnDones: "+  turnDones[index]); 
+
+					dist = Vector3.Distance (carPosition, interPositions [index]);
+					print (index + " : " + interPositions [index]);
+
 					if (car.transform.position.x > interPositions[len-1].x &&  car.transform.position.y > interPositions[len-1].y 
 						&& car.transform.position.z > interPositions[len-1].z) {
 						Time.timeScale = 0;
 					}
-						if (interPositions [index].x < interPositions [index + 1].x) {//turn right x
-							if (dist <= 23f && (turnDones [index] == false)) {
-								v = 0;
-								h = 0.36f;
-								isTurns [index] = true;
-								print ("dang re");
-							} 
-							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-								m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
-								v = 1;
-								h = 0;
-								print ("turn right x: " + index);
-								turnDones [index] = true;
-								index++;
 
-								dist_pre = 999999f;
-								print ("turnDone");
-								//				isTurn = false;
-							}
-						} else if (interPositions [index].x > interPositions [index - 1].x) {// turn left x
-							if (dist <= 23f && (turnDones [index] == false)) {
-								v = 0;
-								h = -0.36f;
-								isTurns [index] = true;
-								print ("dang re");
-							} 
-							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-								m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
-								v = 1;
-								h = 0;
-								print ("turn left x: " + index);
-								turnDones [index] = true;
-								index++;
-								if (index < 0) {
-//									Time.timeScale = 0;
-								}
-								dist_pre = 999999f;
-								print ("turnDone");
-								//				isTurn = false;
-							}
-						} else if (interPositions [index].z < interPositions [index - 1].z) {// turn left z
-							if (dist <= 23f && (turnDones [index] == false)) {
-								v = 0;
-								h = -0.36f;
-								isTurns [index] = true;
-								print ("dang re");
-							} 
-							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-								m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
-								v = 1;
-								h = 0;
-								print ("turn left z: " + index);
-								turnDones [index] = true;
-								index++;
-								if (index < 0) {
-									Time.timeScale = 0;
-								}
-								dist_pre = 999999f;
-								print ("turnDone");
-								//				isTurn = false;
-							}
-						} else if (interPositions [index].z > interPositions [index - 1].z) {// turn right z
-							if (dist <= 23f && (turnDones [index] == false)) {
-								v = 0;
-								h = 0.36f;
-								isTurns [index] = true;
-								print ("dang re");
-							} 
-							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-								m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
-								v = 1;
-								h = 0;
-								print ("turn right z: " + index);
-								turnDones [index] = true;
-								index++;
-								if (index < 0) {
-									Time.timeScale = 0;
-								}
-								dist_pre = 999999f;
-								print ("turnDone");
-								//				isTurn = false;
-							}
-						}
+					logicDirection (interPositions [index], interPositions [index + 1]);
+				
+//					if (interPositions [index].x < interPositions [index + 1].x) {//turn right x
+//						if (dist <= 23f && (turnDones [index] == false)) {
+//							v = 0;
+//							h = 0.36f;
+//							isTurns [index] = true;
+//							print ("dang re");
+//						} 
+//						if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//							m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
+//							v = 1;
+//							h = 0;
+//							print ("turn right x: " + index);
+//							turnDones [index] = true;
+//							index++;
+//
+//							dist_pre = 999999f;
+//							print ("turnDone");
+//							//				isTurn = false;
+//						}
+//					} else if (interPositions [index].x > interPositions [index + 1].x) {// turn left x
+//						if (dist <= 23f && (turnDones [index] == false)) {
+//							v = 0;
+//							h = -0.36f;
+//							isTurns [index] = true;
+//							print ("dang re");
+//						} 
+//						if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//							m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
+//							v = 1;
+//							h = 0;
+//							print ("turn left x: " + index);
+//							turnDones [index] = true;
+//							index++;
+//							if (index < 0) {
+//	//									Time.timeScale = 0;
+//							}
+//							dist_pre = 999999f;
+//							print ("turnDone");
+//							//				isTurn = false;
+//						}
+//					} else if (interPositions [index].z < interPositions [index + 1].z) {// turn left z
+//						if (dist <= 23f && (turnDones [index] == false)) {
+//							v = 0;
+//							h = -0.36f;
+//							isTurns [index] = true;
+//							print ("dang re");
+//						} 
+//						if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//							m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
+//							v = 1;
+//							h = 0;
+//							print ("turn left z: " + index);
+//							turnDones [index] = true;
+//							index++;
+//							if (index < 0) {
+//								Time.timeScale = 0;
+//							}
+//							dist_pre = 999999f;
+//							print ("turnDone");
+//							//				isTurn = false;
+//						}
+//					} else if (interPositions [index].z > interPositions [index +1 ].z) {// turn right z
+//						if (dist <= 23f && (turnDones [index] == false)) {
+//							v = 0;
+//							h = 0.36f;
+//							isTurns [index] = true;
+//							print ("dang re");
+//						} 
+//						if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//							m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
+//							v = 1;
+//							h = 0;
+//							print ("turn right z: " + index);
+//							turnDones [index] = true;
+//							index++;
+//							if (index < 0) {
+//								Time.timeScale = 0;
+//							}
+//							dist_pre = 999999f;
+//							print ("turnDone");
+//							//				isTurn = false;
+//						}
+//					}
 
 
-						dist_pre = dist;
-						// pass the input to the car!
+					dist_pre = dist;
+					// pass the input to the car!
 
-						//			print (h);
-						//			print (v);
-						#if !MOBILE_INPUT
-						float handbrake = CrossPlatformInputManager.GetAxis ("Jump");
-						m_Car.Move (h, v, v, handbrake);
-						#else
-						m_Car.Move(h, v, v, 0f);
-						#endif
+					//			print (h);
+					//			print (v);
+					#if !MOBILE_INPUT
+					float handbrake = CrossPlatformInputManager.GetAxis ("Jump");
+					m_Car.Move (h, v, v, handbrake);
+					#else
+					m_Car.Move(h, v, v, 0f);
+					#endif
 					}
-				}
+			}
 				
 		}
 
@@ -231,6 +230,125 @@ namespace UnityStandardAssets.Vehicles.Car
 		private void ShowMain() {
 			overview_camera.enabled = false;
 			main_camera.enabled = true;
+		}
+
+		private void logicDirection(Vector3 current,Vector3 next){
+
+			if (huong_z == true) {// dang di theo huong z
+				if (current.z < next.z) {// tiep tuc di thang
+					v = 1;
+					h = 0;
+				}
+
+				if (current.x < next.x) { // re phai theo chieu z
+					
+					if (dist <= 23f && (turnDones [index] == false)) {
+						v = 0;
+						h = 0.36f;
+						isTurns [index] = true;
+						print ("dang re");
+					} 
+					if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+						m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
+						v = 1;
+						h = 0;
+						print ("re phai theo chieu z: " + index);
+						turnDones [index] = true;
+						index++;
+
+						dist_pre = 999999f;
+						print ("turnDone");
+						huong_z = false;
+						//				isTurn = false;
+					}
+				}
+				// chua test
+				if (current.x > next.x) { // re trai theo chieu z
+					
+					if (dist <= 23f && (turnDones [index] == false)) {
+						v = 0;
+						h = -0.36f;
+						isTurns [index] = true;
+						print ("dang re");
+					} 
+					if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+						m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
+						v = 1;
+						h = 0;
+						print ("turn left x: " + index);
+						turnDones [index] = true;
+						index++;
+						if (index < 0) {
+//									Time.timeScale = 0;
+						}
+						dist_pre = 999999f;
+						print ("turnDone");
+						huong_z = false;
+						//				isTurn = false;
+					}
+				}
+
+			} 
+			if(huong_z == false){// huong theo chieu x
+				print("theo chieu x");
+				if (current.x < next.x) {// tiep tuc di thang
+					print("di thang x");
+					v = 1;
+					h = 0;
+				}
+
+//				if (current.z < next.z) {// re phai theo x
+//					print("re phai theo chieu x");
+//					huong_z=true;
+//					if (dist <= 23f && (turnDones [index] == false)) {
+//						v = 0;
+//						h = 0.36f;
+//						isTurns [index] = true;
+//						print ("dang re");
+//					} 
+//					if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//						m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
+//						v = 1;
+//						h = 0;
+//						print ("turn right z: " + index);
+//						turnDones [index] = true;
+//						index++;
+//						if (index < 0) {
+//							Time.timeScale = 0;
+//						}
+//						dist_pre = 999999f;
+//						print ("turnDone");
+//						//				isTurn = false;
+//					}
+//				}
+//
+//				if (current.z > next.z) {// re trai theo x
+//					print("re trai x");
+//					huong_z=true;
+//					if (dist <= 23f && (turnDones [index] == false)) {
+//						v = 0;
+//						h = -0.36f;
+//						isTurns [index] = true;
+//						print ("dang re");
+//					} 
+//					if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+//						m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
+//						v = 1;
+//						h = 0;
+//						print ("turn left z: " + index);
+//						turnDones [index] = true;
+//						index++;
+//						if (index < 0) {
+//							Time.timeScale = 0;
+//						}
+//						dist_pre = 999999f;
+//						print ("turnDone");
+//						//				isTurn = false;
+//					}
+//				}
+
+			}
+
 		}
 
 		IEnumerator GetMap(int start,int target)
