86e1aed59465bda0180d19037249d39278dc7040
NguyenTienThao <nguyentienthao@Thaos-MacBook-Pro.local>
2018-05-06 19:45:30 +0700

    get api success

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets.Vehicles.Car
 	[RequireComponent(typeof (CarController))]
 	public class CarUserControl : MonoBehaviour
 	{
+		public Dropdown m_Dropdown;
 
 		bool flag_chuong_ngai_vat=false;
 		Ray ray;
@@ -30,12 +31,13 @@ namespace UnityStandardAssets.Vehicles.Car
 		int index;
 		Vector3 carPosition;
 		int[] checkpoint = new int[20];
+
 		private void Awake()
 		{
-			
+			checkpoint [0] = -1;
 			ShowOverView ();
 			ShowMain ();
-			StartCoroutine(GetText(0,4));
+//			StartCoroutine(GetText(0,4));
 			// get the car controller
 			m_Car = GetComponent<CarController>();
 			inters= GameObject.FindGameObjectsWithTag("inter");
@@ -53,146 +55,152 @@ namespace UnityStandardAssets.Vehicles.Car
 			
 		private void FixedUpdate()
 		{
-			if (flag_chuong_ngai_vat == true) {
+			if (m_Dropdown.value != 0) {
+				Destroy (m_Dropdown);
+				StartCoroutine(GetMap(0,m_Dropdown.value));
+				print ("target:" + m_Dropdown.value);
+				if(checkpoint[0]!=-1)	{
+						if (flag_chuong_ngai_vat == true) {
+
+							timeLeft -= Time.deltaTime;
+							timer.GetComponent<Text> ().text = " " + timeLeft.ToString ();
+						} else {
+							timer.GetComponent<Text> ().text = " ";
+						}
+						ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+						if (timeLeft < 0) {
+							GameObject cnt = GameObject.Find ("chuong_ngai_vat(Clone)");
+							print ("destroy");
+							Destroy (cnt);
+							timeLeft = 10.0f;
+							flag_chuong_ngai_vat = false;
+						}
+						if (Physics.Raycast (ray, out hit)) {
+
+							if (Input.GetKey (KeyCode.Mouse0) && flag_chuong_ngai_vat == false) {
+								GameObject obj = Instantiate (chuong_ngai_vat, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+								flag_chuong_ngai_vat = true;
+
+							}
+
+						}
+
+						float h = 0;// am re phai, duong re trai
+						float v = 1;
+						//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+						//			float v = CrossPlatformInputManager.GetAxis("Vertical");
+						carPosition = car.transform.position;
+
+
+						float dist = Vector3.Distance (carPosition, interPositions [index]);
+						print (index + " : " + interPositions [index]);
+
+						//			print("Distance to other: " + dist);
+						//			print (" h: " + h + " v: "+ v +"distance: "+ dist + " pre_dist:" + dist_pre + " isTurn " + isTurns[index] );
+						//			print (car.transform.position.x);
+						//			print("dist: " +dist + "turnDones: "+  turnDones[index]); 
+
+						if (interPositions [index].x < interPositions [index - 1].x) {//turn right x
+							if (dist <= 23f && (turnDones [index] == false)) {
+								v = 0;
+								h = 0.36f;
+								isTurns [index] = true;
+								print ("dang re");
+							} 
+							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+								m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
+								v = 1;
+								h = 0;
+								print ("turn right x: " + index);
+								turnDones [index] = true;
+								index--;
+								if (index < 0) {
+									Time.timeScale = 0;
+								}
+								dist_pre = 999999f;
+								print ("turnDone");
+								//				isTurn = false;
+							}
+						} else if (interPositions [index].x > interPositions [index - 1].x) {// turn left x
+							if (dist <= 23f && (turnDones [index] == false)) {
+								v = 0;
+								h = -0.36f;
+								isTurns [index] = true;
+								print ("dang re");
+							} 
+							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+								m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
+								v = 1;
+								h = 0;
+								print ("turn left x: " + index);
+								turnDones [index] = true;
+								index--;
+								if (index < 0) {
+									Time.timeScale = 0;
+								}
+								dist_pre = 999999f;
+								print ("turnDone");
+								//				isTurn = false;
+							}
+						} else if (interPositions [index].z < interPositions [index - 1].z) {// turn left z
+							if (dist <= 23f && (turnDones [index] == false)) {
+								v = 0;
+								h = -0.36f;
+								isTurns [index] = true;
+								print ("dang re");
+							} 
+							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+								m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
+								v = 1;
+								h = 0;
+								print ("turn left z: " + index);
+								turnDones [index] = true;
+								index--;
+								if (index < 0) {
+									Time.timeScale = 0;
+								}
+								dist_pre = 999999f;
+								print ("turnDone");
+								//				isTurn = false;
+							}
+						} else if (interPositions [index].z > interPositions [index - 1].z) {// turn right z
+							if (dist <= 23f && (turnDones [index] == false)) {
+								v = 0;
+								h = 0.36f;
+								isTurns [index] = true;
+								print ("dang re");
+							} 
+							if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
+								m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
+								v = 1;
+								h = 0;
+								print ("turn right z: " + index);
+								turnDones [index] = true;
+								index--;
+								if (index < 0) {
+									Time.timeScale = 0;
+								}
+								dist_pre = 999999f;
+								print ("turnDone");
+								//				isTurn = false;
+							}
+						}
+
+
+						dist_pre = dist;
+						// pass the input to the car!
+
+						//			print (h);
+						//			print (v);
+						#if !MOBILE_INPUT
+						float handbrake = CrossPlatformInputManager.GetAxis ("Jump");
+						m_Car.Move (h, v, v, handbrake);
+						#else
+						m_Car.Move(h, v, v, 0f);
+						#endif
+					}
+				}
 				
-				timeLeft -= Time.deltaTime;
-				timer.GetComponent<Text> ().text = " " + timeLeft.ToString();
-			} else {
-				timer.GetComponent<Text> ().text = " ";
-			}
-			ray=Camera.main.ScreenPointToRay(Input.mousePosition);
-			if (timeLeft < 0) {
-				GameObject cnt = GameObject.Find ("chuong_ngai_vat(Clone)");
-				print("destroy");
-				Destroy (cnt);
-				timeLeft = 10.0f;
-				flag_chuong_ngai_vat = false;
-			}
-			if(Physics.Raycast(ray,out hit))
-			{
-
-				if(Input.GetKey(KeyCode.Mouse0) && flag_chuong_ngai_vat==false)
-				{
-					GameObject obj=Instantiate(chuong_ngai_vat,new Vector3(hit.point.x,hit.point.y,hit.point.z), Quaternion.identity) as GameObject;
-					flag_chuong_ngai_vat = true;
-
-				}
-
-			}
-
-			float h = 0;// am re phai, duong re trai
-			float v = 1;
-			//			float h = CrossPlatformInputManager.GetAxis("Horizontal");
-			//			float v = CrossPlatformInputManager.GetAxis("Vertical");
-			carPosition = car.transform.position;
-
-
-			float dist = Vector3.Distance(carPosition, interPositions[index]);
-			print (index+" : " +interPositions[index]);
-
-			//			print("Distance to other: " + dist);
-			//			print (" h: " + h + " v: "+ v +"distance: "+ dist + " pre_dist:" + dist_pre + " isTurn " + isTurns[index] );
-			//			print (car.transform.position.x);
-			//			print("dist: " +dist + "turnDones: "+  turnDones[index]); 
-
-			if (interPositions [index].x < interPositions [index - 1].x) {//turn right x
-				if (dist<=23f && (turnDones[index] == false)) {
-					v = 0;
-					h = 0.36f;
-					isTurns[index] = true;
-					print ("dang re");
-				} 
-				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-					m_Car.transform.eulerAngles = (new Vector3 (0, 80, 0));
-					v = 1;
-					h = 0;
-					print ("turn right x: " + index);
-					turnDones [index] = true;
-					index--;
-					if (index < 0) {
-						Time.timeScale = 0;
-					}
-					dist_pre = 999999f;
-					print ("turnDone");
-					//				isTurn = false;
-				}
-			} else if (interPositions [index].x > interPositions [index - 1].x){// turn left x
-				if (dist<=23f && (turnDones[index] == false)) {
-					v = 0;
-					h = -0.36f;
-					isTurns[index] = true;
-					print ("dang re");
-				} 
-				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-					m_Car.transform.eulerAngles = (new Vector3 (0, -90, 0));
-					v = 1;
-					h = 0;
-					print ("turn left x: " + index);
-					turnDones [index] = true;
-					index--;
-					if (index < 0) {
-						Time.timeScale = 0;
-					}
-					dist_pre = 999999f;
-					print ("turnDone");
-					//				isTurn = false;
-				}
-			}else if (interPositions [index].z < interPositions [index - 1].z){// turn left z
-				if (dist<=23f && (turnDones[index] == false)) {
-					v = 0;
-					h = -0.36f;
-					isTurns[index] = true;
-					print ("dang re");
-				} 
-				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-					m_Car.transform.eulerAngles = (new Vector3 (0, 27, 0));
-					v = 1;
-					h = 0;
-					print ("turn left z: " + index);
-					turnDones [index] = true;
-					index--;
-					if (index < 0) {
-						Time.timeScale = 0;
-					}
-					dist_pre = 999999f;
-					print ("turnDone");
-					//				isTurn = false;
-				}
-			}else if (interPositions [index].z > interPositions [index - 1].z){// turn right z
-				if (dist<=23f && (turnDones[index] == false)) {
-					v = 0;
-					h = 0.36f;
-					isTurns[index] = true;
-					print ("dang re");
-				} 
-				if (dist > (dist_pre) && dist > 20f && isTurns [index]) {
-					m_Car.transform.eulerAngles = (new Vector3 (0, 170, 0));
-					v = 1;
-					h = 0;
-					print ("turn right z: " + index);
-					turnDones [index] = true;
-					index--;
-					if (index < 0) {
-						Time.timeScale = 0;
-					}
-					dist_pre = 999999f;
-					print ("turnDone");
-					//				isTurn = false;
-				}
-			}
-
-
-			dist_pre = dist;
-			// pass the input to the car!
-
-			//			print (h);
-			//			print (v);
-			#if !MOBILE_INPUT
-			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-			m_Car.Move(h, v, v, handbrake);
-			#else
-			m_Car.Move(h, v, v, 0f);
-			#endif
 		}
 
 		private void ShowOverView() {
@@ -205,9 +213,9 @@ namespace UnityStandardAssets.Vehicles.Car
 			main_camera.enabled = true;
 		}
 
-		IEnumerator GetText(int start,int target)
+		IEnumerator GetMap(int start,int target)
 		{
-			String url = "http://127.0.0.1:3000/:" + start + "/:" + target;
+			String url = "http://127.0.0.1:3000/" + start + "/" + target;
 			using (UnityWebRequest www = UnityWebRequest.Get(url))
 			{
 				yield return www.Send();
@@ -227,7 +235,7 @@ namespace UnityStandardAssets.Vehicles.Car
 					String str = System.Text.Encoding.Default.GetString(results);
 
 					str = str.Substring (1,str.Length-2);
-//					print ("res: " + str);
+					print ("checkpoints: " + str);
 					String[] trace = str.Split(","[0]);
 					for(int i= 0; i < trace.Length; i++){
 //						print (trace [i]);
